935c26687435c17b7c3cf53c0286eedcef8daf8a
reecedantin <reecedantin@yahoo.com>
2018-11-30 16:08:22 -0500

    added get user info

--- lambdaFunctions/GTMLogin/index.js
+++ lambdaFunctions/GTMLogin/index.js
@@ -16,6 +16,9 @@ exports.handler = (event, context, callback) => {
     } else if(event.path == "/GTMLogin/linkspotify") {
         console.log("Linking spotify")
         linkSpotify(event, callback)
+    } else if(event.path == "/GTMLogin/getuserinfo") {
+        console.log("Getting user info")
+        getUserInfo(event, callback)
     } else {
         //handle(event, callback)
         console.log("UNHANDLED EVENT PATH: " + event.path)
@@ -203,72 +206,125 @@ function linkSpotify(event, callback) {
       json: true
     };
 
+    console.log("getting user access token")
+
     request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var access_token = body.access_token,
                 refresh_token = body.refresh_token,
                 expiresAt = Date.now() + body.expires_in;
 
-            //first we gotta find which user has this token then use that username(primary key) to update the info
-            var params = {
-                ExpressionAttributeNames: {
-                    "#T": "token"
+
+            var authOptions = {
+                url: 'https://api.spotify.com/v1/me',
+                headers: {
+                'Authorization': 'Bearer ' + access_token
                 },
-                ExpressionAttributeValues: {
-                    ":t": {
-                        S: token
-                    }
-                },
-                FilterExpression: "#T = :t",
-                TableName: "MusicUsers"
+                json: true
             };
 
-            dynamodb.scan(params, function(err, data) {
-                var response = {
-                    statusCode: 404,
-                    headers: {
-                        "Access-Control-Allow-Origin" : "*",
-                        'Content-Type' : 'text/html'
-                    },
-                    body: "failed"
-                };
+            console.log("got access token");
 
-                if (err) {
-                    console.log(err, err.stack); // an error occurred
-                    callback(null, response)
-                } else {
-                    if(data["Count"] == 1) {
-                        console.log("found userid: " + data["Items"][0].userid.S)
-                        var params = {
-                            ExpressionAttributeNames: {
-                                "#C": "spotifyInfo"
+            request.get(authOptions, function(error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var display_name = body.display_name
+                    var user_id = body.id
+
+                    var image_url = "default"
+
+                    if(body.images[0] != null) {
+                        image_url = body.images[0].url
+                    }
+
+
+                    var premium = (body.product == "premium")
+
+                    console.log("got user id " + user_id);
+
+                    //first we gotta find which user has this token then use that username(primary key) to update the info
+                    var params = {
+                        ExpressionAttributeNames: {
+                            "#T": "token"
+                        },
+                        ExpressionAttributeValues: {
+                            ":t": {
+                                S: token
+                            }
+                        },
+                        FilterExpression: "#T = :t",
+                        TableName: "MusicUsers"
+                    };
+
+                    dynamodb.scan(params, function(err, data) {
+                        var response = {
+                            statusCode: 404,
+                            headers: {
+                                "Access-Control-Allow-Origin" : "*",
+                                'Content-Type' : 'text/html'
                             },
-                            ExpressionAttributeValues: {
-                                ":r": {
-                                    S: refresh_token
-                                },
-                                ":a": {
-                                    S: access_token
-                                },
-                                ":x": {
-                                    N: "" + expiresAt
-                                }
-                            },
-                            Key: {
-                                "userid": {
-                                    S: data["Items"][0].userid.S
-                                },
-                            },
-                            ReturnValues: "UPDATED_NEW",
-                            TableName: "MusicUsers",
-                            UpdateExpression: "SET #C.refresh_token = :r, #C.access_token = :a, #C.expiresAt = :x"
+                            body: "failed"
                         };
 
-                        console.log("Linking Spotify...token: " + token);
-                        dynamodb.updateItem(params, function(err, data) {
-                            if (err) {
-                                console.log(err, err.stack); // an error occurred
-                                callback(new Error("error"), response)
+                        if (err) {
+                            console.log(err, err.stack); // an error occurred
+                            callback(null, response)
+                        } else {
+                            if(data["Count"] == 1) {
+                                console.log("found userid: " + data["Items"][0].userid.S)
+                                var params = {
+                                    ExpressionAttributeNames: {
+                                        "#C": "spotifyInfo"
+                                    },
+                                    ExpressionAttributeValues: {
+                                        ":r": {
+                                            S: refresh_token
+                                        },
+                                        ":a": {
+                                            S: access_token
+                                        },
+                                        ":x": {
+                                            N: "" + expiresAt
+                                        },
+                                        ":d": {
+                                            S: display_name
+                                        },
+                                        ":i": {
+                                            S: user_id
+                                        },
+                                        ":m": {
+                                            S: image_url
+                                        },
+                                        ":p": {
+                                            BOOL: premium
+                                        }
+                                    },
+                                    Key: {
+                                        "userid": {
+                                            S: data["Items"][0].userid.S
+                                        },
+                                    },
+                                    ReturnValues: "UPDATED_NEW",
+                                    TableName: "MusicUsers",
+                                    UpdateExpression: "SET #C.refresh_token = :r, #C.access_token = :a, #C.expiresAt = :x, #C.display_name = :d, #C.id = :i, #C.image_url = :m, #C.premium = :p"
+                                };
+
+                                console.log("Linking Spotify...token: " + token);
+                                dynamodb.updateItem(params, function(err, data) {
+                                    if (err) {
+                                        console.log(err, err.stack); // an error occurred
+                                        callback(new Error("error"), response)
+                                    } else {
+                                        response = {
+                                            statusCode: 200,
+                                            headers: {
+                                                "Access-Control-Allow-Origin" : "*",
+                                                'Content-Type' : 'text/html'
+                                            },
+                                            body: "success"
+                                        };
+                                        callback(null, response)
+                                    }
+                                });
                             } else {
                                 response = {
                                     statusCode: 200,
@@ -276,33 +332,101 @@ function linkSpotify(event, callback) {
                                         "Access-Control-Allow-Origin" : "*",
                                         'Content-Type' : 'text/html'
                                     },
-                                    body: "success"
+                                    body: "failed"
                                 };
-                                callback(null, response)
+                                callback(null, response);
                             }
-                        });
-                    } else {
-                        response = {
-                            statusCode: 200,
-                            headers: {
-                                "Access-Control-Allow-Origin" : "*",
-                                'Content-Type' : 'text/html'
-                            },
-                            body: "failed"
-                        };
-                        callback(null, response);
-                    }
 
+                        }
+
+                    });
                 }
-
-             });
+            })
+        } else {
+            console.log(response)
         }
     })
 
 
+}
+
+function getUserInfo(event, callback) {
+    var data = querystring.parse(event.body);
+    var token = data.token
+    console.log("finding user with token: " + token)
+    console.log(event.body)
+    var params = {
+        ExpressionAttributeNames: {
+            "#T": "token"
+        },
+        ExpressionAttributeValues: {
+            ":t": {
+                S: token
+            }
+        },
+        FilterExpression: "#T = :t",
+        TableName: "MusicUsers"
+    };
+
+    dynamodb.scan(params, function(err, data) {
+        var response = {
+            statusCode: 404,
+            headers: {
+                "Access-Control-Allow-Origin" : "*",
+                'Content-Type' : 'text/html'
+            },
+            body: "failed"
+        };
+
+        if (err) {
+            console.log(err, err.stack); // an error occurred
+            callback(null, response)
+        } else {
+            if(data["Count"] == 1) {
+                console.log("found userid: " + data["Items"][0].userid.S)
+
+
+                var returnData = {}
+
+                //spotify not linked
+                if(data["Items"][0].spotifyInfo.M.access_token.S == "null") {
+                    returnData.display_name = data["Items"][0].userid.S
+                    returnData.image_url = "default"
+                    returnData.premium = false
+                } else {
+                    returnData.display_name = data["Items"][0].spotifyInfo.M.display_name.S
+                    returnData.image_url = data["Items"][0].spotifyInfo.M.image_url.S
+                    returnData.premium = data["Items"][0].spotifyInfo.M.premium.BOOL
+                }
 
 
 
+                response = {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin" : "*",
+                        'Content-Type' : 'text/html'
+                    },
+                    body: JSON.stringify(returnData)
+                };
+                callback(null, response);
+
+            } else {
+                console.log("counldnt find")
+                response = {
+                    statusCode: 200,
+                    headers: {
+                        "Access-Control-Allow-Origin" : "*",
+                        'Content-Type' : 'text/html'
+                    },
+                    body: "failed"
+                };
+                callback(null, response);
+            }
+
+        }
+
+    });
 }
 
 
@@ -334,7 +458,7 @@ function hashPassword(password) {
 }
 
 function generateToken(data) {
-    return data + "+" +  Math.random().toString(36).substring(7);
+    return data + "%" +  Math.random().toString(36).substring(7);
 }
 
 // hashPassword('PASSWORD');
